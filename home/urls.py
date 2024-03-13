a3306671adc150b711ff21a37702c44b76f2ce0a
jig <johngavinroser@gmail.com>
2024-03-13 17:46:55 -0500

    changed paths

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.ClassStats.as_view(), name='index'),
     path('weather',views.Weather.as_view(), name="weather"),
     path('shortcuts',views.Shortcuts.as_view(), name="shortcuts"),
     path('schedule',views.Schedule.as_view(), name="schedule"),
