a34603015f4a73ca03be4b6e39661232fffe4ebe
jig <johngavinroser@gmail.com>
2024-03-14 10:57:53 -0500

    suppressing value eroro

--- core/settings.py
+++ core/settings.py
@@ -199,6 +199,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+WHITENOISE_MANIFEST_STRICT = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
