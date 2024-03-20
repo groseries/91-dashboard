b0b19e8890af0ea872cb3c9572bcb0aae3430274
jig <johngavinroser@gmail.com>
2024-03-19 19:22:32 -0500

    added antfw domain

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ if not SECRET_KEY:
 # Render Deployment Code
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["nine1-dashboard.onrender.com"]
+ALLOWED_HOSTS = ["nine1-dashboard.onrender.com","91antfw.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
